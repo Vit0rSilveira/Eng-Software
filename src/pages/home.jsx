ba43e29aabf413fbbe8cfb2769394de670143540
Vit0rSilveira <vitor.silveira98@gmail.com>
2024-04-29 00:09:31 -0300

    Ajustando a home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from '../components/header';
-import Footer from '../components/footer';
 import Question from "../components/questions";
+import Footer from '../components/footer';
 import '../styles/pages/home.css'
 
 function Home() {
@@ -27,31 +27,39 @@ function Home() {
         <>
             <Header />
             <div id="carrosel"></div>
-            <div id="about-us" className="box-text">
-                <h1>Um pouco sobre nós</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda consectetur sed aliquid libero! Aliquid deserunt laudantium suscipit maxime inventore recusandae esse eos sunt nesciunt porro ut, iste vel perferendis quaerat.</p>
-            </div>
-
-            <div id="last-updates"  className="box-text">
-                <h1>Últimas Ações Realizadas</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda consectetur sed aliquid libero! Aliquid deserunt laudantium suscipit maxime inventore recusandae esse eos sunt nesciunt porro ut, iste vel perferendis quaerat.</p>
-            </div>
-
-            <div id="questions"  className="box-text">
-                <h1>Perguntas Frequentes</h1>
-                <div id="box-questions">
-                    {questions.slice(0, 4).map((q, index) => (
-                        <Question
-                            key={index}
-                            question={q.question}
-                            answer={q.answer}
-                            className = "one-question"
-                        />
-                    ))}
+            <main>
+                <div id="about-us" className="box-text">
+                    <h1>Um pouco sobre nós</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda consectetur sed aliquid libero! Aliquid deserunt laudantium suscipit maxime inventore recusandae esse eos sunt nesciunt porro ut, iste vel perferendis quaerat.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus nam ducimus iste eveniet rem. Voluptatum veniam at doloribus atque cum odio minima nemo error? Neque quaerat dicta sint quisquam!
+                    </p>
                 </div>
 
-            </div>
-            <Footer/>
+                <div id="last-updates" className="box-text">
+                    <h1>Últimas Ações Realizadas</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda consectetur sed aliquid libero! Aliquid deserunt laudantium suscipit maxime inventore recusandae esse eos sunt nesciunt porro ut, iste vel perferendis quaerat.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi dolorem error excepturi nemo doloremque? Maxime, nihil, mollitia accusamus assumenda earum, nulla quia rem quibusdam voluptatibus natus totam molestias provident saepe.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit maiores qui enim quia. Ipsam, quidem quis suscipit non delectus cupiditate, ut inventore debitis numquam quam enim doloribus voluptate sunt laborum?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit distinctio, saepe placeat tempore debitis recusandae amet, porro omnis vel eveniet commodi illo non totam inventore, sit doloremque excepturi quas?
+                    </p>
+                </div>
+
+                <div id="questions" className="box-text">
+                    <h1>Perguntas Frequentes</h1>
+                    <div id="box-questions">
+                        {questions.slice(0, 4).map((q, index) => (
+                            <Question
+                                key={index}
+                                question={q.question}
+                                answer={q.answer}
+                                className="one-question"
+                            />
+                        ))}
+                    </div>
+
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
