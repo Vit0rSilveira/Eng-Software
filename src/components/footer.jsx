74457d7720c158a28b3cfe168064e8934f3722fd
g-faccini <gabriel_faccini@hotmail.com>
2024-05-01 20:56:53 -0300

    Responsividade do footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,29 +3,28 @@ import "../styles/components/footer.css"
 import { FaFacebook,FaInstagram} from "react-icons/fa";
 import { FaPhone } from "react-icons/fa6";
 import { IoLocationSharp } from "react-icons/io5";
-import { Link } from "react-router-dom";
 /*  */
 function Footer() {
     return (
         <>
         <footer>
             <div class='redes'>
-                <p class="redes-text" >Acesse nossas redes</p>
+                <p class="contrast-text media-hide" >Acesse nossas redes:</p>
                 <div class='redes-icons'>
                     <FaInstagram size={30}/>
                     <FaFacebook size={30}/>
                 </div>
             </div>
             <div class='contato'>
-                <p> Entre em contato conosco</p>
+                <p class='contrast-text media-hide'> Entre em contato conosco:</p>
                 <p> paofraterno@gmail.com </p>
-                <FaPhone size={30} />
+                <div class='media-hide'><FaPhone size={30} /></div>
             </div>
             <div class='localizacao'>
-                <p><IoLocationSharp size={30} />Localização:</p>
+                <p class='contrast-text media-hide'><IoLocationSharp size={30} />Localização:</p>
                 <p>Rua Alguma Coisa, Bairro não sei o que lá</p>
                 <p>Num: 123</p>
-                <p>Cep: 123.456-78</p>
+                <p class='media-hide'>Cep: 123.456-78</p>
             </div>
         </footer>
         </>
