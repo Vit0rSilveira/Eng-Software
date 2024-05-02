4d99061981fece8c2126d73844027438c0a79422
Marina Figueiredo <108432075+marinasfigueiredo@users.noreply.github.com>
2024-05-02 17:01:36 -0300

    ajustando telas

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from '../components/header';
 import Question from "../components/questions";
 import Footer from '../components/footer';
-import '../styles/pages/home.css'
+import '../styles/pages/cadastro.css'
 
 function Cadastro() {
     const [questions, setQuestions] = useState([]);
@@ -27,20 +27,26 @@ function Cadastro() {
         <>
             <Header />
         
-            <accounts>
-                <div id="questions" className="box-questions">
-                    <div id="box-questions">
-                        {questions.slice(0, 4).map((q, index) => (
-                            <Question
-                                key={index}
-                                question={q.question}
-                                answer={q.answer}
-                                className="one-question"
-                            />
-                        ))}
+            <div class="container">
+                <div class="row mt-5 mb-5">
+                    
+                    <div class=" border border-dark rounded offset-1 col-4 bg-light p-5 mb-5" >
+                        <img className="img-x" src = "../../public/images/marca-cruzada.png" alt="X" id="imagem-x" />
+                        <p>Thiago Ribeiro Correia</p>
+                        <p>ThiagoRibeiroCorreia@rhyta.com</p>
+                        <p>978-761-7423</p>
+                        <p>Mozilla/5.0 (Windows NT 10.0; Win64; x64)</p>
+                        <p>AppleWebKit/537.36</p>
+                        <p>(KHTML, like Gecko)Chrome/74.0.3729.169</p>
+                        Safari/537.36
                     </div>
+                    <div class=" border border-dark rounded offset-1 col-4 bg-light p-5 mb-5">Coluna 2</div>
+
+                    <div class=" border border-dark rounded offset-1 col-4 bg-light p-5 ">Coluna 1</div>
+                    <div class=" border border-dark rounded offset-1 col-4 bg-light p-5">Coluna 2</div>
                 </div>
-            </accounts>
+            </div>
+
             <Footer />
         </>
     )
