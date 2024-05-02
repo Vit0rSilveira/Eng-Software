73fa5c40c58527f1255955dd1f265a485b0130dc
g-faccini <gabriel_faccini@hotmail.com>
2024-05-01 22:03:23 -0300

    Adicionado responsividade ao header e footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import "../styles/components/header.css"
-import { Link } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Header() {
 
+    function handleMenuClick(){
+        let a = document.getElementsByName('menu')[0];
+        let clss = a.classList[0];
+        if(clss == 'menu-onclick-hidden'){
+            a.classList.remove('menu-onclick-hidden');
+            a.classList.add('menu-onclick-show');
+            return;
+        }
+        a.classList.remove('menu-onclick-show');
+        a.classList.add('menu-onclick-hidden');
+        
+    }
+
     return (
+        <>
         <header>
             <div id="logo">
-                <img src="../../public/images/logo.jpg" alt="Logotipo do Pão Fraterno" id="logo-pao-fraterno"/>
-                <h4>Grupo Pão Fraterno</h4>
+                <a href='/'><img src="../../public/images/logo.jpg" alt="Logotipo do Pão Fraterno" id="logo-pao-fraterno"/></a>
+                <h4 >Grupo Pão Fraterno</h4>
                 <h4>André Luiz</h4>
             </div>
-            <ul>
-                <li>Quem Somos</li>
-                <li>Notícias</li>
-                <li>Doação</li>
-                <li>Seja um Voluntário</li>
-                <li>Seja um patrocinador</li>
-                </ul>
+            <ul class='collapse-xl'>
+                <li> <a href='#'>Quem somos</a></li>
+                <li> <a href='#'>Notícias</a></li>
+                <li> <a href='#'>Doação</a></li>
+                <li> <a href='/voluntariado'>Seja um voluntário</a></li>
+                <li> <a href='/patrocinadores'>Seja um patrocinador</a></li>
+            </ul>
+            <div class='show-xl'>
+                <RxHamburgerMenu onClick= {handleMenuClick} size={60}/>
+            </div>
         </header>
+        <ul name='menu' class='menu-onclick-hidden'>
+                <a href='#'><li>Quem Somos</li></a>
+                <a href='#'><li>Notícias</li></a>
+                <a href='#'><li>Doação</li></a>
+                <a href='/voluntariado'><li>Seja um Voluntário</li></a>
+                <a href='/patrocinadores'><li>Seja um patrocinador</li></a>
+        </ul>
+        </>
 
     )
 }
