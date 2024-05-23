19dd5d0c4e0d76181be084a94b221997b198ec71
884kaito <kaito.hayashi@usp.br>
2024-05-23 20:09:14 -0300

    alteracao no header 1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,14 +15,12 @@ function Header() {
         <header>
             <div id="logo">
                 <a href='/'><img src="../../public/images/logo2.jpg" alt="Logotipo do Pão Fraterno" id="logo-pao-fraterno"/></a>
-                <h4 >Pão Fraterno</h4>
-                <h4>André Luiz</h4>
             </div>
             <ul class='collapse-xl header-ul'>
                 <li> <a href='/info'>Quem somos</a></li>
-                <li> <a href='/acoes'>Notícias</a></li>
                 <li> <a href='/doacao'>Doação</a></li>
                 <li> <a href='/voluntariado'>Seja voluntário</a></li>
+                <li> <a href='/acoes'>Notícias</a></li>
                 <li> <a href='/patrocinadores'>Colaboradores</a></li>
             </ul>
             <div class='show-xl'>
@@ -31,9 +29,9 @@ function Header() {
         </header>
         <ul name='menu' class='header-ul menu-onclick'>
                 <a href='/info'><li>Quem Somos</li></a>
-                <a href='/acoes'><li>Notícias</li></a>
                 <a href='/doacao'><li>Doação</li></a>
                 <a href='/voluntariado'><li>Seja um Voluntário</li></a>
+                <a href='/acoes'><li>Notícias</li></a>
                 <a href='/patrocinadores'><li>Patrocinadores</li></a>
         </ul>
         </>
