8250b9960064eab613e25a9488bbf294f4c8e31b
Vit0rSilveira <vitor.silveira98@gmail.com>
2024-06-02 12:06:29 -0300

    Finalizando o CRUD do usuário

--- backend/src/routes/userRotas.js
+++ backend/src/routes/userRotas.js
@@ -3,6 +3,29 @@ import User  from '../models/User.js'
 
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+        const usuarios = await User.find()
+
+        return res.status(200).json({usuarios : usuarios})
+    }
+    catch(erro) {
+        return res.status(500).json({erro : erro})
+    }
+})
+
+router.get('/:login', async (req, res) => {
+    try {
+        const login = req.params.login;
+        const usuario = await User.findOne({login: login})
+
+        return res.status(200).json({usuarios : usuario})
+    }
+    catch(erro) {
+        return res.status(500).json({erro : erro})
+    }
+})
+
 router.post('/', async (req, res) => {
     const {login, senha, texto_auxiliar} = req.body
 
@@ -26,6 +49,48 @@ router.post('/', async (req, res) => {
         return res.status(500).json({erro: erro})
     }
 })
+
+
+router.patch("/:login", async (req, res) => {
+    const loginBuscado = req.params.login
+    const {login, senha, texto_auxiliar} = req.body
+
+    const user = {
+        login,
+        senha,
+        texto_auxiliar
+    }
+
+    try {
+        const updatePerson = await User.updateOne({ login: loginBuscado }, user)
+
+        if (updatePerson.matchedCount === 0)
+            return res.status(422).json({ message: "Usuário não encontrado" })
+
+        return res.status(200).json({ message: "Usuário atualizado com sucesso" })
+    } catch (error) {
+        return res.status(500).json({ error: error })
+    }
+})
+
+router.delete("/:login", async (req, res) => {
+    const login = req.params.login;
+
+    try {
+        const usuario = await User.findOne({ login: login });
+
+        if (!usuario) {
+            return res.status(422).json({ message: "Usuário não encontrado" });
+        }
+
+        await User.deleteOne({ login: login });
+
+        return res.status(200).json({ message: "Usuário removido com sucesso" });
+        
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
 
 const userRotas = router;
 export default userRotas
