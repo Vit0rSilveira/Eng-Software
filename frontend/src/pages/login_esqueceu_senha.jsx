0fb45915ddb23cdee9a6a0095520cdbbf88315ab
884kaito <kaito.hayashi@usp.br>
2024-06-03 15:48:12 -0300

    envio de email no login

--- frontend/src/pages/login_esqueceu_senha.jsx
+++ frontend/src/pages/login_esqueceu_senha.jsx
@@ -1,7 +1,36 @@
 import '../styles/pages/login.css'
 import '../styles/pages/login_esqueceu_senha.css'
+import emailjs from '@emailjs/browser'
 
 function LoginEsqueceuSenha(){
+
+    //envia email com texto de auxilio
+    //Tutorial: https://www.youtube.com/watch?v=Lz8yx-zn-O4
+    const sendEmail = () =>{
+        
+        // envia email com texto de auxilio
+        // var emailParams = {
+        //     toEmail: 'exemplo@gmail.com',
+        //     fromEmail: 'exemplo@gmail.com',
+        //     textoAuxilio: 'TextoTeste'
+        //   };
+        // var service_ID = coloque o service ID aqui
+        // var template_ID = coloque o template ID aqui
+        // var public_key = coloque o public_key aqui
+        // emailjs.send('service_ID', 'template_iq7cld8', emailParams, 'public_key').then(
+        //     (response) => {
+        //         console.log('SUCCESS!', response.status, response.text);
+        //         window.location.href = "/login-email-enviado"
+        //     },
+        //     (error) => {
+        //         console.log('FAILED...', error);
+        //     },
+        // )
+            
+            
+            //comentar essa parte depois
+            window.location.href = "/login-email-enviado"
+    }
 
     return(
         <>
@@ -22,7 +51,7 @@ function LoginEsqueceuSenha(){
                     texto auxilio da senha?
                 </div>
                 <div>
-                    <button onClick={() => (window.location.href = "/login-email-enviado")} className="defaultButton" id="botaoLoginConf">
+                    <button onClick={() => sendEmail()} className="defaultButton" id="botaoLoginConf">
                         Enviar
                     </button>
                     <button onClick={() => (window.location.href = "/login")} className="defaultButton" id="botaoLoginConf">
