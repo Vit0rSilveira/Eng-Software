785d56b3de21315f9a4a8747d40f12339b3c1ef2
Marina Figueiredo <108432075+marinasfigueiredo@users.noreply.github.com>
2024-06-04 19:36:47 -0300

    conserto pagina doacao

--- frontend/src/pages/doacao.jsx
+++ frontend/src/pages/doacao.jsx
@@ -7,52 +7,65 @@ import '../styles/pages/voluntariado.css';
 
 function Doacao() {
   return (
-    <div className="doacao-container">
+    <div className="doacao">
       <Header 
-        headerText1 = {'Cadastro de Voluntários'}
-        isHomePage = {true}
+        headerText1 = {'Formas de Ajudar'}
+        isHomePage = {false}
       />
       <div id = "conteinerMaster">
-                <div class = "titulo">Veja o que você consegue fazer!</div>
+                <div class = "titulo">Produtos de Doação</div>
                 <div id = "cardMargin">
                     <DoacaoCard
                         color = "#F1C144"
-                        image = "public\images\cadastroVoluntario\alimento.png"
-                        title = "Doação de produtos"
+                      
+                        title = "Alimento"
                         text = "Aceitamos doação de alimento, roupa, produtos de higiene e outros"
                     />
                     <div id="last"><DoacaoCard
                         color = "#FFBA52"
-                        image = "public\images\cadastroVoluntario\sopa.png"
-                        title = "Preparo do kit de doação"
+                    
+                        title = "Recipiente"
                         text = "Temos tranalho de cozinhar, montagem de kit e entrega"
                     /></div>
                     <DoacaoCard
                         color = "#FFBA52"
-                        image = "public\images\cadastroVoluntario\carro.png"
-                        title = "Doação a partir da casa"
+                        
+                        title = "Produtos de Higiene"
                         text = "Retiramos a doação na sua casa"
                     />
                     <DoacaoCard
                         color = "#F1C144"
-                        image = "public\images\cadastroVoluntario\casa.png"
-                        title = "Visita no Pão Fraterno"
+                        
+                        title = "Dinheiro"
                         text = "Venha conhecer nossa casa!"
                     />
                 </div>
-
-        <h1>Como doar?</h1>
-            <p>Aceitamos doacoes de alimentos, cobertores, roupas, produtos de limpeza e descartaveis</p>
-            <p>Também aceitamos doações em dinheiro, seja no cartão ou pix!</p>
-            <p>Sua doação é muito importante para continuarmos nosso caminho de fraternidade!</p>
-              <a href='/voluntariado'>  <p>Clique aqui para doar!</p></a>
-            
-        <div className="image-container">
-          <h1>Doação por pix</h1>
-          <img src="../public/images/qr.png" alt="Imagem qr code" />
-          <div className="text-under">Nome: Joazinho da Silva<br/> Chave Aleatória: dcta478j-196l-03fm-t6gh-4298er7845m2<br /></div>
-
-        </div>
+                <div class = "titulo">Formas de Doação</div>
+                <div id = "cardMargin">
+                <DoacaoCard
+                        color = "#FFBA52"
+                        
+                        title = "Doação Direta"
+                        text = "Doação Direta
+                                Doe direto na casa do Pão Fraterno!
+                                Endereço: 
+                                Rua Costa do Sol, 450 Vila Costa do Sol São Carlos - SP 13566-070
+                                Horário: 
+                                Escrever os horários de atendimento"
+                    />
+                    <DoacaoCard
+                        color = "#F1C144"
+                        
+                        title = "Doação pela casa"
+                        text = "Doação pela Casa
+                                Doe sem sair de sua casa!
+                                Procedimento: 
+                                Escreva o endereço e horário no formulário abaixo, e nós vamos retirar o produto
+                                Horário: 
+                                Escrever os horários de atendimento"
+                    />
+                </div>
+       
       </div>
       <Footer />
     </div>
