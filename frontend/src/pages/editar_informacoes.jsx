3e0336c18031e04d73bc8086b2f4f5936802be0b
g-faccini <gabriel_faccini@hotmail.com>
2024-06-06 19:05:44 -0300

    componente evento e mudancas na pagina editar site

--- frontend/src/pages/editar_informacoes.jsx
+++ frontend/src/pages/editar_informacoes.jsx
@@ -1,40 +1,73 @@
 import HeaderADM from '../components/header_adm.jsx';
+import Evento from '../components/evento.jsx';
 import '../styles/pages/editar_informacoes.css';
 import React, {useState} from 'react';
 
 function Edit_Info(){
-    
-    const [selection,setSelection] = useState('eventos');
-
-    let btnEventosClass = '';
-    let btnColaboradoresClass='';
-
-
-    if(selection == 'eventos')
-        btnEventosClass = 'selected-btn';
-    else if(selection == 'colaboradores')
-        btnColaboradoresClass = 'selected-btn';
-
-    // callback btn adicionar
-    function handleBtnAdicionar(){
-        alert("Adicionado com sucesso!");
-    }
-
     function Form(props){
+        let eventoValues = {
+            titulo: "",
+            data: "",
+            horario_inicio: "",
+            horario_fim: "",
+            endereco: "",
+        };
 
+        let colaboradorValues ={}
+        
         function handleFileSelection(){
             let  nome_arquivo = document.getElementById('selecionar-arquivo').files[0].name;
             let p = document.getElementById('input-field-fileName')
             p.innerText = nome_arquivo
         }
+
+        function handleBtnAdicionar(selection){
+            if(selection == 'eventos'){
+                const {titulo,data,horario_inicio,horario_fim,endereco} = eventoValues;
+                console.log(titulo,data,horario_inicio,horario_fim,endereco);
+                if(!(titulo && data && horario_fim && horario_inicio && endereco)){
+                    alert("Preencha todos os campos");
+                    return;
+                }
+                setEventos([...eventos,{...eventoValues}]);
+            }
+            else if(selection == 'colaboradores'){
+                alert('todo!');
+                return;
+            }
+            else{
+                console.log('unreachable');
+            }
+            eventoValues = {};
+            alert("Adicionado com sucesso!");
+        }
+
+        function handleNomeChange(event){
+            eventoValues.titulo=event.target.value;
+        }
+
+        function handleDataChange(event){
+            eventoValues.data = event.target.value;
+        }
+
+        function handleHorarioChange(event,inicio){
+            inicio ? eventoValues.horario_inicio = event.target.value
+                   : eventoValues.horario_fim = event.target.value 
+        }
+
+        function handleEnderecoChange(event){
+            eventoValues.endereco = event.target.value;
+        }
+
         let selection = props.selection;
+        
         if(selection == 'eventos'){
             return(
                 <div className ='container-edit'>
                     <h1 className="Titulo">Adicionar Evento</h1>
                     <div>
                         <h2>Nome:</h2>
-                        <input type='text'></input>
+                        <input maxlength="50" onChange={handleNomeChange} type='text'></input>
                     </div>
                     <div>
                         <h2>Imagem:</h2>
@@ -44,19 +77,21 @@ function Edit_Info(){
                     </div>
                     <div>
                         <h2>Data:</h2>
-                        <input type="date" className='input-data'></input>
+                        <input  onChange={handleDataChange}type="date"  className='input-data'></input>
                     </div>
                     <div>
                         <h2>Horário:</h2>
-                        <input type="time" className='input-horario'></input>
-                        <p className='inline-text'>até</p>
-                        <input type="time" className='input-horario'></input>
+                        <section>
+                            <input onChange={(evento) => handleHorarioChange(evento,true)} type="time" className='input-horario'></input>
+                            <p className='inline-text'>até</p>
+                            <input onChange={(evento) => handleHorarioChange(evento,false)} type="time" className='input-horario'></input>
+                        </section>
                     </div>
                     <div>
                         <h2>Endereço:</h2>
-                        <input></input>
+                        <input  maxlength="100"onChange={handleEnderecoChange}></input>
                     </div>
-                    <button className='button-adicionar' onClick={handleBtnAdicionar}>Adicionar</button>
+                    <button className='button-adicionar' onClick={()=>handleBtnAdicionar('eventos')}>Adicionar</button>
                 </div>
             )
         }
@@ -79,13 +114,63 @@ function Edit_Info(){
                         <input type='text'></input>
                     </div>
                     <div className='content'></div>
-                    <button className='button-adicionar' onClick={handleBtnAdicionar}>Adicionar</button>
+                    <button className='button-adicionar' onClick={()=>handleBtnAdicionar('colaboradores')}>Adicionar</button>
                     
 
                 </div>
             )
         }
     }
+
+    let [selection,setSelection] = useState('eventos');
+    let [eventos,setEventos] = useState([
+        {
+            titulo: "Brechó de roupas",
+            data: "11/05/2024",
+            horario_inicio: "12:00",
+            horario_fim: "15:00",
+            endereco: "Rua Costa do Sol, 450 - Vila Costa do Sol, São Carlos - SP, 13566-070",
+        },
+        {
+            titulo: "Venda de pizza",
+            data: "19/05/2024",
+            horario_inicio: "11:00",
+            horario_fim: "16:00",
+            endereco: "Rua Costa do Sol, 450 - Vila Costa do Sol, São Carlos - SP, 13566-070",
+        }
+    ]);
+
+    let [colaboradores,setColaboradores] = useState([
+        {
+            titulo: "Brechó de roupas",
+            data: "11/05/2024",
+            horario_inicio: "12:00",
+            horario_fim: "15:00",
+            endereco: "Rua Costa do Sol, 450 - Vila Costa do Sol, São Carlos - SP, 13566-070",
+        },
+        {
+            titulo: "Venda de pizza",
+            data: "19/05/2024",
+            horario_inicio: "11:00",
+            horario_fim: "16:00",
+            endereco: "Rua Costa do Sol, 450 - Vila Costa do Sol, São Carlos - SP, 13566-070",
+        }
+    ]);
+
+    let btnEventosClass = '';
+    let btnColaboradoresClass='';
+
+
+    if(selection == 'eventos')
+        btnEventosClass = 'selected-btn';
+    else if(selection == 'colaboradores')
+        btnColaboradoresClass = 'selected-btn';
+
+    function removeEvento(index){
+        eventos.splice(index,1)
+        setEventos([...eventos]);
+    }
+
 
     return(
         <>
@@ -97,8 +182,23 @@ function Edit_Info(){
                     <button className={btnColaboradoresClass} onClick= {()=> setSelection('colaboradores')}>Colaboradores</button>
                 </div>
                 <Form selection={selection}/>
-                
             </div>
+            {selection == 'eventos' 
+                ? 
+                <div id="container-lista-eventos">
+                {eventos.map((evento,index)=> 
+                    <Evento id={index} endereco={evento.endereco} titulo={evento.titulo} data={evento.data}
+                    horario_fim={evento.horario_fim} horario_inicio={evento.horario_inicio} onClick={removeEvento}/>)}
+                </div>
+
+                : 
+                
+                <div id='container-lista-colaboradores'>
+                    
+                </div>
+            }
+            
+           
         </>
     )
 }
