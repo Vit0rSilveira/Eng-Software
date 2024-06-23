5acdd343cc5189c8df94e6b7de69b54f348add4f
884kaito <kaito.hayashi@usp.br>
2024-06-23 20:37:31 -0300

    colocando textos no home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -53,38 +53,61 @@ function Home() {
             <main>
                 <div className="textoBranco box-text">
                     <h1>Um pouco sobre nós</h1>
-                    <p>Somos uma fraternidade de São Carlos que surgiu nos anos 90, criada pelos próprios alunos da USP de São Carlos.
-                    Começamos como um grupo que aceitava apenas alimentos basicos, roupas e cobertores. 
+                    <p>O Pão Fraterno André Luiz é uma organização que tem como objetivo ajudar, 
+                        fisicamente e mentalmente os moradores de rua do São Carlos. 
+                        Somos um grupo de aproximadamente 40 pessoas, sem fins lucrativos, com caráter de assistência social.
                     </p>
-                    <img
-                    className="img-fluid"
-                    src="../../public/images/quem_somos.png"
-                    alt="First slide"
-                    />
-                    <p>Hoje em dia aceitamos os mais
-                    diversos alimentos, o nosso foco sao verduras, que é essencial para fazer nossa principal refeição, a sopa, 
-                    distribuída todas as sextas-feiras. Aceitamos produtos de higiene básica como papel higienico e sabonetes, tambem
-                    recolhemos roupas para o inverno ou não, roupas intimas, cobertores e mantas. Recebemos como muita alegria e gratidão
-                    sua doação.
+                    <div className="home-img-box">
+                        <img
+                        className="img-fluid"
+                        src="../../public/images/quem_somos.png"
+                        alt="First slide"
+                        />
+                    </div>
+                    
+                    <p>
+                        Hoje em dia aceitamos os mais
+                        diversos alimentos, o nosso foco sao verduras, que é essencial para fazer nossa principal refeição, a sopa, 
+                        distribuída todas as sextas-feiras. Aceitamos produtos de higiene básica como papel higienico e sabonetes, tambem
+                        recolhemos roupas para o inverno ou não, roupas intimas, cobertores e mantas. Recebemos como muita alegria e gratidão
+                        sua doação.
                     </p>
                 </div>
 
                 <div className="textoBranco box-text orange">
                     <h1>Ações Realizadas</h1>
-                    <p>As ações voluntarias que realizamos podem ser divididas em 4 grupos distintos de atuação. Um deles é a doação, 
-                    que é a fonte de todas as nossas ações. As demais se baseiam em atividades que realizamos cotidianamente para 
-                    manter o funcionamento da Pão Fraterno. Ou seja, fazer os alimentos, realizar sua montagem para distribuicao, ir
-                    as ruas recolher doacoes, ir em bairros que atendemos e distribuir. Todas essas constituem pilares essenciais que 
-                    manteem a fraternidade em pe. </p>
-                    <img
-                    className="img-fluid"
-                    src="../../public/images/entrega_doacoes.png"
-                    alt="First slide"
-                    />
+                    <p>
+                    A nossa principal atividade é a entrega de kit para 140 moradores de rua na sexta toda semana sem falta. 
+                    Nesse kit temos alimentos, roupas e alguns produtos de higiene.
+                    
+                    </p>
+                    <p>
+                    As ações voluntárias para a entrega podem ser divididas em 4 grupos distintos de atuação. Um deles é a doação, 
+                    que é a fonte de todas as nossas ações. 
+                    </p>
+                    <p>
+                    As demais se baseiam em atividades que realizamos cotidianamente para 
+                    manter o funcionamento da Pão Fraterno. Ou seja, fazer os alimentos, realizar sua montagem para distribuiçao, ir
+                    as casas dos doadores recolher as doações, ir em bairros que atendemos e distribuir. Todas essas constituem pilares essenciais que 
+                    mantem a fraternidade em pé. 
+                    </p>
+                        
+                    <div className="home-img-box">
+                        <img
+                        className="img-fluid"
+                        src="../../public/images/entrega_doacoes.png"
+                        alt="First slide"
+                        />
+                    </div>
+                    
+                    <p>
+                        Além disso temos fazemos eventos aproximadamente 4 vezes por ano, para arrecadar o dinheiro para compra de produtos do kit. 
+                    Dentre eles, temos eventos como venda de pizza, brechó e etc.
+                    Qualquer pessoa está convida para ir nos eventos.
+                    </p>
                     <p>Por mais que pareçam muitas funcoes buscamos sempre dividi-las ao maximo, o que ainda 
                     reforca nossa necessidade constante de voluntariado, por isso buscamos atençāo para a questao social a qual nos 
-                    envolvemos e esperamos sua ajuda para fazer parte da nossa história. placeat tempore debitis recusandae
-                    met, porro omnis vel eveniet commodi illo non totam inventore, sit doloremque excepturi quas?</p>
+                    envolvemos e esperamos sua ajuda para fazer parte da nossa história.</p>
                     
                 </div>
 
@@ -99,11 +122,13 @@ function Home() {
                             />
                         ))}
                     </div>
-                    <img
-                    className="img-fluid"
-                    src="../../public/images/cenario_doacao.png"
-                    alt="First slide"
-                    />
+                    <div className="home-img-box">
+                        <img
+                        className="img-fluid"
+                        src="../../public/images/cenario_doacao.png"
+                        alt="First slide"
+                        />  
+                    </div>
                 </div>
                 
                 {/* Lista de eventos */}
