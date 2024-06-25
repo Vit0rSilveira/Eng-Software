2f552d37bad708026a1a0377d5725575237842ed
Vit0rSilveira <vitor.silveira98@gmail.com>
2024-06-25 20:59:55 -0300

    Cansei de escrever

---
+++ frontend/src/utils/constantes.js
@@ -0,0 +1,7 @@
+const constantes = {SERVICEID: 'service_g5jlksr',
+    TEMPLATEID: 'template_3dvyvay',
+    TEMPLATEID_VOLUNTARIO: 'template_hfwr2cn',
+    PUBLICKEY_EMAILJS: 'MNSL_oHJGMEmOQZaW',
+    PATH: 'http://localhost:3000'}
+
+export default constantes;
