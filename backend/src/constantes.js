600e335dab16a2b3806606eb58e92e7294c40a78
884kaito <kaito.hayashi@usp.br>
2024-07-30 16:32:07 -0300

    Update .gitignore

--- backend/src/constantes.js
+++
@@ -1,6 +0,0 @@
-//Modifique aqui com seus constantes
-const constantes = {PORT: --sua porta para o backend--,
-    USER_MONGODB: --login do mongoDB--,
-    PASSWORD_MONGODB: --senha do mongoDB--}
-
-export default constantes;
--- frontend/src/utils/constantes.js
+++
@@ -1,8 +0,0 @@
-//Modifique aqui com seus constantes
-const constantes = {SERVICEID: --service id do emailJS--,
-    TEMPLATEID: --id do template geral do emailJS--,
-    TEMPLATEID_VOLUNTARIO: --id do template voluntario do  emailJS--,
-    PUBLICKEY_EMAILJS: --public key do emailJS,
-    PATH: --seu link para o frontend--}
-
-export default constantes;
